7c406d590b22e3860607b7c9a87192aed0f41fc6
Antonio Colella <antoniocolella80@gmail.com>
2025-02-19 19:06:27 +0100

    aggiornati file

--- Manipolazione-del-dom/index.js
+++ Manipolazione-del-dom/index.js
@@ -1,17 +1,3 @@
-// function addProduct() {
-//   const taskInput = document.getElementById("taskInput");
-//   const taskList = document.getElementById("taskList");
-//   const taskText = taskInput.value.trim();
-
-
-//     const li = document.createElement("li");
-//     const checkbox = document.createElement("input");
-//     checkbox.type = "checkbox";
-
-//     li.appendChild(checkbox);
-//     li.appendChild(document.createTextNode(taskText));
-//     taskList.appendChild(li);
-// }
 
 
 function addProduct() {
