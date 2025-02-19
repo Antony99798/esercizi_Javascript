d5db8c44d0e64086051d92022ce2572eb5e83e98
Antonio Colella <antoniocolella80@gmail.com>
2025-02-19 19:05:25 +0100

    aggiornati file

--- Manipolazione-del-dom/index.js
+++ Manipolazione-del-dom/index.js
@@ -1,17 +1,28 @@
+// function addProduct() {
+//   const taskInput = document.getElementById("taskInput");
+//   const taskList = document.getElementById("taskList");
+//   const taskText = taskInput.value.trim();
+
+
+//     const li = document.createElement("li");
+//     const checkbox = document.createElement("input");
+//     checkbox.type = "checkbox";
+
+//     li.appendChild(checkbox);
+//     li.appendChild(document.createTextNode(taskText));
+//     taskList.appendChild(li);
+// }
+
+
 function addProduct() {
-  const taskInput = document.getElementById("taskInput");
-  const taskList = document.getElementById("taskList");
-  const taskText = taskInput.value.trim();
+    const ul = document.querySelector("ul");
+    const input = document.querySelector("input")
 
+    const li = document.createElement("li")
+    const checkbox = document.createElement("input")
+    li.innerHTML = input.value
+    checkbox.setAttribute("type", "checkbox");
 
-    const li = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-
-    li.appendChild(checkbox);
-    li.appendChild(document.createTextNode(taskText));
-    taskList.appendChild(li);
-
-
-  
+    ul.appendChild(li)
+    li.appendChild(checkbox)
 }
